81454b10088f21e1dd5b95312b63c88e26184190
francois vallat <vallatfrancois@yahoo.fr>
2023-03-08 16:28:50 +0100

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  end
 end
